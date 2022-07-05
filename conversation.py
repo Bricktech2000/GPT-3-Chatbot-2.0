f617fda2f4c34449750d94fce1b9b45c982e22c5
Bricktech <bricktech2000@gmail.com>
2022-07-05 19:44:26 -0400

    improved code structure and added `INIT` command support

--- conversation.py
+++ conversation.py
@@ -2,15 +2,36 @@ class Conversation:
   def __init__(self, conversation_timeout, messages=[], last_message_timestamp=0):
     self.conversation_timeout = conversation_timeout
     self.messages = messages
-    self.last_messsage_timestamp = last_message_timestamp
+    self.last_message_timestamp = last_message_timestamp
 
   def with_message(self, name, content, timestamp):
-    is_conversation_timed_out = self.last_messsage_timestamp + \
+    is_conversation_timed_out = self.last_message_timestamp + \
         self.conversation_timeout < timestamp
     init_message = [f'{name}: INIT'] if is_conversation_timed_out else []
-    new_message = [f'{name}: {content}']
+    new_message = [f'{name}: {content}'] if content else []
 
     return Conversation(self.conversation_timeout, self.messages + init_message + new_message, timestamp)
 
-  def get_conversation(self, length):
+  def without_last(self):
+    return Conversation(self.conversation_timeout, self.messages[:-1], self.last_message_timestamp)
+
+  def get_last_messages(self, length):
     return '\n'.join(self.messages[-length:])
+
+
+class ConversationManager:
+  def __init__(self, conversation_timeout):
+    self.conversations = {}
+    self.conversation_timeout = conversation_timeout
+
+  def get(self, *ids):
+    conversation_id = '\n'.join(map(str, ids))
+
+    if conversation_id not in self.conversations:
+      self.conversations[conversation_id] = Conversation(
+          self.conversation_timeout)
+
+    def save(conversation):
+      self.conversations[conversation_id] = conversation
+
+    return self.conversations[conversation_id], save
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import math
 import discord
 import openai
 import asyncio
 import sys
 import re
 import time
+import random
 from conversation import Conversation
+from conversation import ConversationManager
 
 if len(sys.argv) != 4:
   print("Usage: python main.py <Discord bot token> <OpenAI API key> <OpenAI model name")
@@ -14,6 +17,7 @@ if len(sys.argv) != 4:
 intents = discord.Intents.default()
 intents.members = True
 intents.presences = True
+intents.reactions = True
 client = discord.Client(intents=intents)
 
 BOT_TOKEN = sys.argv[1]
@@ -47,51 +51,52 @@ def name_from_member(member):
 
 
 def function_from_command(command=None):
-  async def do_react(message, save, arg):
-    save()
+  async def do_react(message, conversation, save, arg):
+    save(conversation)
     await message.add_reaction(arg)
 
-  async def do_reply(message, save, arg):
-    save()
+  async def do_reply(message, conversation, save, arg):
+    save(conversation)
     await message.reply(arg)
 
-  async def do_sleep(message, save, arg):
-    save()
+  async def do_sleep(message, conversation, save, arg):
     # https://stackoverflow.com/questions/49286640/how-to-set-bots-status
     # https://stackoverflow.com/questions/65773693/how-to-set-an-invisible-status-using-discord-py
-    try:
-      if arg.endswith('m'):
-        time = int(arg[:-1]) * 60
-      elif arg.endswith('h'):
-        time = int(arg[:-1]) * 60 * 60
-      else:
-        time = 0
-    except ValueError:
-      time = 0
 
+    min, max = 30, 1 * 60 * 60  # 5 seconds to 3 hours
+    # make it more likely to sleep for less time
+    n = 10
+    delay = math.pow(random.uniform(math.pow(min, 1/n), math.pow(max, 1/n)), n)
+
+    print(f'bot is sleeping for {round(delay)}s\n\n')
     await client.change_presence(status=discord.Status.offline)
-    print('bot is sleeping')
-    await asyncio.sleep(time)
+    await asyncio.sleep(delay)
     await client.change_presence(status=discord.Status.online)
-    print('bot is awake')
+    print(f'bot is awake\n\n')
+
+    conversation = conversation.without_last().with_message(
+        name_from_member(client.user), '', time.time())
+
+    save(conversation)
+    await execute(message, conversation, save)
 
   def do_log(text):
-    async def func(message, save, arg):
+    async def func(message, conversation, save, arg):
       await message.channel.send(f'**COMMAND: {text}** {arg}')
     return func
 
-  async def do_send(message, save, arg):
-    save()
+  async def do_send(message, conversation, save, arg):
+    save(conversation)
     async with message.channel.typing():
       typing_delay = len(arg) / TYPING_SPEED
       await asyncio.sleep(typing_delay)
     await message.channel.send(arg)
 
-  async def do_nothing(message, save, arg):
+  async def do_nothing(message, conversation, save, arg):
     pass
 
   COMMAND_MAP = {
-      'INIT;': do_nothing,
+      'INIT;': do_sleep,
       'SAVE;': do_log('save last message'),
       'REACT: ': do_react,
       'REPLY: ': do_reply,
@@ -123,28 +128,38 @@ def GPT_3(messages):
   return str(response_object['choices'][0]['text'])[1:]
 
 
-@client.event
+@ client.event
 async def on_ready():
   print(f"Logged in as {client.user}")
 
 
-conversations = {}
+conversation_manager = ConversationManager(CONVERSATION_TIMEOUT)
 
 
-@client.event
+@ client.event
+async def on_reaction_add(reaction, user):
+  message = reaction.message
+  conversation, save = conversation_manager.get(
+      message.channel.id, message.guild.id)
+
+  if user.id != client.user.id:
+    conversation = conversation.with_message(
+        name_from_member(user), f'REACT: {reaction.emoji}', time.time())
+
+  save(conversation)
+
+  await execute(reaction.message, conversation, save)
+
+
+@ client.event
 async def on_message(message):
-  conversation_id = f'{message.channel.id}%{message.guild.id}'
-  conversation = conversations.get(
-      conversation_id, Conversation(CONVERSATION_TIMEOUT))
+  conversation, save = conversation_manager.get(
+      message.channel.id, message.guild.id)
 
   # https://stackoverflow.com/questions/12597370/python-replace-string-pattern-with-output-of-function
   def replacer(match):
     return name_from_member(message.guild.get_member(int(match.group(1))))
   content = re.sub(r'<@(.+?)>', replacer, message.content.replace('\n', ' '))
-
-  # TODO: media / pictures
-
-  # TODO: auto wake after inactivity and sleeping
 
   if message.author.id != client.user.id and content != '':
     conversation = conversation.with_message(
@@ -152,19 +167,28 @@ async def on_message(message):
   for attachment in message.attachments:
     conversation = conversation.with_message(
         name_from_member(message.author), 'PHOTO;', time.time())
-  conversations[conversation_id] = conversation
+
+  save(conversation)
+
+  await execute(message, conversation, save)
+
+
+async def execute(message, conversation, save):
+  # TODO: media / pictures
+
+  # TODO: auto wake after inactivity and sleeping
 
   if message.guild.get_member(client.user.id).status != discord.Status.online:
     # return if the bot is sleeping
     return
 
   prediction = GPT_3(
-      conversation.get_conversation(NUM_MESSAGES) + '\n\n')
+      conversation.get_last_messages(NUM_MESSAGES) + '\n\n')
 
-  if content.startswith('OVERRIDE: '):
-    prediction = f"{name_from_member(client.user)}: {content.replace('OVERRIDE: ', '')}"
+  if message.content.startswith('OVERRIDE: '):
+    prediction = f"{name_from_member(client.user)}: {message.content.replace('OVERRIDE: ', '')}"
 
-  print(conversation.get_conversation(NUM_MESSAGES) + '\n\n', end='')
+  print(conversation.get_last_messages(NUM_MESSAGES) + '\n\n', end='')
   print(prediction, end='')
   print('\n\n')
 
@@ -175,12 +199,15 @@ async def on_message(message):
     res_name, command, arg = match.group(1), \
         match.group(2), match.group(3)
 
-    if res_name == name_from_member(client.user):
+    if res_name == name_from_member(client.user) or command == 'INIT;':
       # GPT-3 predicts bot should take action
-      def save():
-        conversations[conversation_id] = conversations[conversation_id].with_message(
-            name_from_member(client.user), f'{command}{arg}', time.time())
-      await function_from_command(command)(message, save, arg)
+      conversation, save = conversation_manager.get(
+          message.channel.id, message.guild.id)
+
+      conversation = conversation.with_message(
+          name_from_member(client.user), f'{command}{arg}', time.time())
+
+      await function_from_command(command)(message, conversation, save, arg)
 
 
 openai.api_key = API_KEY
