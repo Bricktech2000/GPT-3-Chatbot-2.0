b7f36bf0ce3790b58d86425b748ff79ce2e5a5eb
Bricktech <bricktech2000@gmail.com>
2022-07-05 03:21:07 -0400

    improved code structure and added new features

--- conversation.py
+++ conversation.py
@@ -1,10 +1,6 @@
-import time
-
-
 class Conversation:
-  def __init__(self, conversation_timeout, channel, messages=[], last_message_timestamp=0):
+  def __init__(self, conversation_timeout, messages=[], last_message_timestamp=0):
     self.conversation_timeout = conversation_timeout
-    self.channel = channel
     self.messages = messages
     self.last_messsage_timestamp = last_message_timestamp
 
@@ -14,7 +10,7 @@ class Conversation:
     init_message = [f'{name}: INIT'] if is_conversation_timed_out else []
     new_message = [f'{name}: {content}']
 
-    return Conversation(self.conversation_timeout, self.channel, self.messages + init_message + new_message, timestamp)
+    return Conversation(self.conversation_timeout, self.messages + init_message + new_message, timestamp)
 
   def get_conversation(self, length):
     return '\n'.join(self.messages[-length:])
--- main.py
+++ main.py
@@ -25,62 +25,77 @@ MAX_TOKENS = 50  # the maximal length of GPt-3's answers
 TYPING_SPEED = 15  # characters per second
 CONVERSATION_TIMEOUT = 120  # seconds, time for a conversation to be considered dead
 
-# TODO: move out of this file
-NAME_MAP = {
-    296473451190026240: 'max',
-    362627068430909450: 'emilien',
-    349323309357465619: 'alex',
-    423516682548412418: 'reid',
-    381859631481487361: 'amelie',
-    334409678387806208: 'cameron',
-    426167359665995777: 'edouard',
-    529778786330345483: 'jaya',
-    858772258394996768: 'kiera',
-    573621663753568306: 'courtney',
-    0: 'grace'
-}
+
+def name_from_member(member):
+  # TODO: move out of this file
+  NAME_MAP = {
+      296473451190026240: 'max',
+      362627068430909450: 'emilien',
+      349323309357465619: 'alex',
+      423516682548412418: 'reid',
+      381859631481487361: 'amelie',
+      334409678387806208: 'cameron',
+      426167359665995777: 'edouard',
+      529778786330345483: 'jaya',
+      858772258394996768: 'kiera',
+      573621663753568306: 'courtney',
+      0: 'grace'
+  }
+  id = member.id if member.id != client.user.id else 0
+  return NAME_MAP.get(id, member.display_name).lower()
 
 
-async def do_react(message, arg): await message.add_reaction(arg)
-async def do_reply(message, arg): await message.reply(arg)
+def function_from_command(command=None):
+  async def do_react(message, arg): await message.add_reaction(arg)
+  async def do_reply(message, arg): await message.reply(arg)
 
-
-async def do_sleep(message, arg):
-  # https://stackoverflow.com/questions/49286640/how-to-set-bots-status
-  try:
-    if arg.endswith('m'):
-      time = int(arg[:-1]) * 60
-    elif arg.endswith('h'):
-      time = int(arg[:-1]) * 60 * 60
-    else:
+  async def do_sleep(message, arg):
+    # https://stackoverflow.com/questions/49286640/how-to-set-bots-status
+    # https://stackoverflow.com/questions/65773693/how-to-set-an-invisible-status-using-discord-py
+    try:
+      if arg.endswith('m'):
+        time = int(arg[:-1]) * 60
+      elif arg.endswith('h'):
+        time = int(arg[:-1]) * 60 * 60
+      else:
+        time = 0
+    except ValueError:
       time = 0
-  except ValueError:
-    time = 0
 
-  client.bot.set_status('invisible')
-  await asyncio.sleep(time)
-  message.channel.send('')
-  client.bot.set_status('available')
+    await client.change_presence(status=discord.Status.offline)
+    print('bot is sleeping')
+    await asyncio.sleep(time)
+    await client.change_presence(status=discord.Status.online)
+    print('bot has woken')
 
+  def do_log(text):
+    async def func(message, arg):
+      await message.channel.send(f'**COMMAND: {text}** {arg}')
+    return func
 
-def do_send(text):
-  async def func(message, arg):
-    await message.channel.send(f'**COMMAND: {text}** {arg}')
-  return func
+  async def do_send(message, arg):
+    async with message.channel.typing():
+      typing_delay = len(arg) / TYPING_SPEED
+      await asyncio.sleep(typing_delay)
+    await message.channel.send(arg)
 
+  async def do_nothing(message, arg):
+    pass
 
-COMMAND_MAP = {
-    'INIT': do_send('initialize conversation'),
-    'SAVE': do_send('save last message'),
-    'REACT': do_react,
-    'REPLY': do_reply,
-    'SLEEP': do_sleep,
-    'SLEPT': do_send('went offline for duration:'),
-    'SAVE': do_send('save last message'),
-    'PHOTO': do_send('send photo:'),
-    'VIDEO': do_send('send video:'),
-    'MEME': do_send('send meme:'),
-}
+  COMMAND_MAP = {
+      'INIT': do_nothing,
+      'SAVE': do_log('save last message'),
+      'REACT': do_react,
+      'REPLY': do_reply,
+      'SLEEP': do_sleep,
+      'SLEPT': do_nothing,
+      'PHOTO': do_log('send photo:'),
+      'VIDEO': do_log('send video:'),
+      'MEME': do_log('send meme:'),
+      'SEND': do_send
+  }
+
+  return COMMAND_MAP[command] if command is not None else COMMAND_MAP
 
 
 def GPT_3(messages):
@@ -101,17 +116,7 @@ def GPT_3(messages):
 
 @client.event
 async def on_ready():
-  global NAME_MAP
-
-  NAME_MAP[client.user.id] = NAME_MAP[0]
-  del NAME_MAP[0]
-
   print(f"Logged in as {client.user}")
-  # print(GPT_3("max: Depends on the drinks\ncameron: For sure jaya \ud83d\udc80\ud83d\udc80\nmax: That was max\ncameron: Implying you are not max?\nmax: I am max\nmax: Max is me\nmax: PHOTO photo of max taken by jaya\nmax: Me\n\n"))
-
-
-def name_from_user(member):
-  return NAME_MAP.get(member.id, member.display_name).lower()
 
 
 conversations = {}
@@ -121,49 +126,45 @@ conversations = {}
 async def on_message(message):
   conversation_id = f'{message.channel.id}%{message.guild.id}'
   conversation = conversations.get(
-      conversation_id, Conversation(CONVERSATION_TIMEOUT, message.channel))
-
-  username = name_from_user(message.author)
+      conversation_id, Conversation(CONVERSATION_TIMEOUT))
 
   # https://stackoverflow.com/questions/12597370/python-replace-string-pattern-with-output-of-function
   def replacer(match):
-    return name_from_user(message.guild.get_member(int(match.group(1))))
+    return name_from_member(message.guild.get_member(int(match.group(1))))
   content = re.sub(r'<@(.+?)>', replacer, message.content.replace('\n', ' '))
 
   # TODO: media / pictures
 
   # TODO: timeout?
 
-  if message.author.id != client.user.id:
+  if content != '':
     conversation = conversation.with_message(
-        username, content, time.time())
+        name_from_member(message.author), content, time.time())
+  for attachment in message.attachments:
+    conversation = conversation.with_message(
+        name_from_member(message.author), 'PHOTO', time.time())
+  conversations[conversation_id] = conversation
 
   prediction = GPT_3(
       conversation.get_conversation(NUM_MESSAGES) + '\n\n')
 
-  print(conversation.get_conversation(
-      NUM_MESSAGES) + '\n\n', end='')
+  if content.startswith('OVERRIDE '):
+    prediction = f"{name_from_member(client.user)}: {content.replace('OVERRIDE ', '')}"
+
+  print(conversation.get_conversation(NUM_MESSAGES) + '\n\n', end='')
   print(prediction, end='')
-  print('\n\n', end='')
+  print('\n\n')
 
-  bot_name = name_from_user(client.user)
-  if prediction.startswith(f'{bot_name}: '):
-    response = prediction.replace(f'{bot_name}: ', '')
-    conversation = conversation.with_message(bot_name, response, time.time())
+  match = re.search(
+      f"^(.+?): (({'|'.join(function_from_command()) }) )?(.*)$", prediction)
+  if match:
+    # prediction is correctly formed
+    res_name, command, arg = match.group(1), \
+        match.group(3) or 'SEND', match.group(4)
 
-    # TODO: refactor code below
-    for command_id, command_fn in COMMAND_MAP.items():
-      if response.startswith(command_id):
-        command_token = f'{command_id} '
-        await command_fn(message, response.replace(command_token, ''))
-        break
-    else:
-      async with message.channel.typing():
-        typing_delay = len(response) / TYPING_SPEED
-        await asyncio.sleep(typing_delay)
-      await message.channel.send(response)
-
-  conversations[conversation_id] = conversation
+    if res_name == name_from_member(client.user):
+      # GPT-3 predicts bot should take action
+      await function_from_command(command)(message, arg)
 
 
 openai.api_key = API_KEY
