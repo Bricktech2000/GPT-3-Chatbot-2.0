605d303c5c32b4925761bf243e47f742ae3293a3
Bricktech <bricktech2000@gmail.com>
2022-07-05 01:41:35 -0400

    created `Conversation` class

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import asyncio
 import sys
 import re
 import time
+from conversation import Conversation
 
 if len(sys.argv) != 4:
   print("Usage: python main.py <Discord bot token> <OpenAI API key> <OpenAI model name")
@@ -18,11 +19,11 @@ BOT_TOKEN = sys.argv[1]
 API_KEY = sys.argv[2]
 ENGINE = sys.argv[3]
 
-NUM_CHATS = 8  # the number of chats to append to the training data
+NUM_MESSAGES = 8  # the number of messages to append to the training data
 TEMPERATURE = 0.8  # the "originality" of GPT-3's answers
 MAX_TOKENS = 50  # the maximal length of GPt-3's answers
 TYPING_SPEED = 15  # characters per second
-CONVERSATION_TIMEOUT = 10  # seconds, time for a conversation to be considered dead
+CONVERSATION_TIMEOUT = 120  # seconds, time for a conversation to be considered dead
 
 # TODO: move out of this file
 NAME_MAP = {
@@ -44,6 +45,24 @@ async def do_react(message, arg): await message.add_reaction(arg)
 async def do_reply(message, arg): await message.reply(arg)
 
 
+async def do_sleep(message, arg):
+  # https://stackoverflow.com/questions/49286640/how-to-set-bots-status
+  try:
+    if arg.endswith('m'):
+      time = int(arg[:-1]) * 60
+    elif arg.endswith('h'):
+      time = int(arg[:-1]) * 60 * 60
+    else:
+      time = 0
+  except ValueError:
+    time = 0
+
+  client.bot.set_status('invisible')
+  await asyncio.sleep(time)
+  message.channel.send('')
+  client.bot.set_status('available')
+
+
 def do_send(text):
   async def func(message, arg):
     await message.channel.send(f'**COMMAND: {text}** {arg}')
@@ -55,7 +74,7 @@ COMMAND_MAP = {
     'SAVE': do_send('save last message'),
     'REACT': do_react,
     'REPLY': do_reply,
-    'SLEEP': do_send('go offline for duration:'),
+    'SLEEP': do_sleep,
     'SLEPT': do_send('went offline for duration:'),
     'SAVE': do_send('save last message'),
     'PHOTO': do_send('send photo:'),
@@ -64,16 +83,10 @@ COMMAND_MAP = {
 }
 
 
-# TODO: implement conversation class
-conversation = []
-last_message_timestamp = 0
-conversation_channel = None
-
-
-def GPT_3(conversation):
+def GPT_3(messages):
   response_object = openai.Completion.create(
       engine=ENGINE,
-      prompt=conversation,
+      prompt=messages,
       temperature=TEMPERATURE,
       max_tokens=MAX_TOKENS,
       top_p=1,
@@ -101,39 +114,44 @@ def name_from_user(member):
   return NAME_MAP.get(member.id, member.display_name).lower()
 
 
+conversations = {}
+
+
 @client.event
 async def on_message(message):
-  global last_message_timestamp
-  global conversation
+  conversation_id = f'{message.channel.id}%{message.guild.id}'
+  conversation = conversations.get(
+      conversation_id, Conversation(CONVERSATION_TIMEOUT, message.channel))
 
   username = name_from_user(message.author)
 
   # https://stackoverflow.com/questions/12597370/python-replace-string-pattern-with-output-of-function
-  def asdf(match):
+  def replacer(match):
     return name_from_user(message.guild.get_member(int(match.group(1))))
-  content = re.sub(r'<@(.+?)>', asdf, message.content.replace('\n', ' '))
+  content = re.sub(r'<@(.+?)>', replacer, message.content.replace('\n', ' '))
 
   # TODO: media / pictures
 
   # TODO: timeout?
 
-  if last_message_timestamp + CONVERSATION_TIMEOUT < time.time():
-    conversation.append(f'{username}: INIT')
   if message.author.id != client.user.id:
-    conversation.append(f'{username}: {content}')
-  last_message_timestamp = time.time()
-  conversation_channel = None
+    conversation = conversation.with_message(
+        username, content, time.time())
 
-  prediction = GPT_3('\n'.join(conversation[-NUM_CHATS:]) + '\n\n')
+  prediction = GPT_3(
+      conversation.get_conversation(NUM_MESSAGES) + '\n\n')
 
-  print('\n'.join(conversation[-NUM_CHATS:]) + '\n\n')
-  print(prediction)
+  print(conversation.get_conversation(
+      NUM_MESSAGES) + '\n\n', end='')
+  print(prediction, end='')
+  print('\n\n', end='')
 
-  start_token = f'{NAME_MAP[client.user.id]}: '
-  if prediction.startswith(start_token):
-    conversation.append(prediction)
-    response = prediction.replace(start_token, '')
+  bot_name = name_from_user(client.user)
+  if prediction.startswith(f'{bot_name}: '):
+    response = prediction.replace(f'{bot_name}: ', '')
+    conversation = conversation.with_message(bot_name, response, time.time())
 
+    # TODO: refactor code below
     for command_id, command_fn in COMMAND_MAP.items():
       if response.startswith(command_id):
         command_token = f'{command_id} '
@@ -144,6 +162,8 @@ async def on_message(message):
         typing_delay = len(response) / TYPING_SPEED
         await asyncio.sleep(typing_delay)
       await message.channel.send(response)
+
+  conversations[conversation_id] = conversation
 
 
 openai.api_key = API_KEY
